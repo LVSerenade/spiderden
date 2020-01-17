751bf29adf07bccb26a598d42de24712984b39b0
Bl0101 <yacine.mahdid@live.com>
2020-01-16 23:18:09 -0500

    remove the redundant spiders

--- pc_part_picker/pc_part_picker/spiders/parts_spider.py
+++ pc_part_picker/pc_part_picker/spiders/parts_spider.py
@@ -14,19 +14,23 @@ def get_data(soup):
 
     return spec_value
 
-class CasePartSpider(scrapy.Spider):
-    name = "case"
-    base_url = 'https://pcpartpicker.com/'
+class AllPartSpider(scrapy.Spider):
+    # Here we overide the default behavior
+    custom_settings = {
+        'FEED_FORMAT': 'json',
+        'FEED_URI' : 'all_parts.json'
+    }
+
+    name = "parts"
+    base_url = 'https://pcpartpicker.com'
     
     # This slug is what will trigger the fetching of the data
     magic_slug = "fetch/?page=%d&xslug=&location=&search=&qid=1&scr=1&scr_vw=1263&scr_vh=319&scr_dw=1280&scr_dh=720&scr_daw=1280&scr_dah=680&scr_ddw=1263&scr_ddh=5806&ms=1578965341903"
-   
-    type_part = "products/case/"
+
 
     start_urls = [
-        base_url + type_part + magic_slug,
+        base_url + "/products/"
     ]
-
 
     link_regex = "product.*\w{1}"
     page_regex = 'page=[0-9]*'
@@ -34,11 +38,23 @@ class CasePartSpider(scrapy.Spider):
     def parse(self, response):
         
         # Get all links in the page
+        all_part_type_links = response.css('.wrapper__pageContent a::attr(href)').getall()
+        
+
+        max_num_page = -1
+        for link in all_part_type_links:
+            part_type = link.split('/')[2]
+            part_type_url = self.base_url + link + self.magic_slug
+            yield scrapy.Request(part_type_url, callback=self.parse_part_type, cb_kwargs=dict(part_type=part_type))
+
+    def parse_part_type(self, response, part_type):
+        
+        # Get all links in the page
         all_links = response.css("a::attr(href)").getall()
 
         max_num_page = -1
         for link in all_links:
-
+            
             # Check if it is a next page link            
             page_search = re.compile(self.page_regex)
             result = page_search.findall(link)
@@ -48,14 +64,13 @@ class CasePartSpider(scrapy.Spider):
                     max_num_page = num_page
 
         # Iterate over all the pages for this product
-        
         for i in range(1,max_num_page+1):
-            new_slug = self.magic_slug % (i)
-            page_url = self.base_url + self.type_part + '/' + new_slug
-            yield scrapy.Request(page_url, callback=self.parse_page)
+            page_url = response.url % (i)
+            print(page_url)
+            yield scrapy.Request(page_url, callback=self.parse_page, cb_kwargs=dict(part_type=part_type))
         
 
-    def parse_page(self, response):
+    def parse_page(self, response, part_type):
         # Get all links in the page
         all_links = response.css("a::attr(href)").getall()
         for link in all_links:
@@ -64,17 +79,14 @@ class CasePartSpider(scrapy.Spider):
             link_search = re.compile(self.link_regex)
             result = link_search.findall(link)
             if result != []:
-                product_url = self.base_url + result[0]
-                yield scrapy.Request(product_url, callback=self.parse_cpu)
+            
+                product_url = self.base_url + '/' + result[0]
+                yield scrapy.Request(product_url, callback=self.parse_part, cb_kwargs=dict(part_type=part_type))
     
-    def parse_cpu(self, response):
-        '''
-            will parse the product page for specification on the computer part
-            need to be called like this:
-            >>> yield scrapy.Request(product_url, callback=self.parse_cpu)
-        '''
+    def parse_part(self, response, part_type):
         # Default of scraped info (nothing in it)
-        scraped_info = {}
+        scraped_info = {
+        }
 
         specs_list = []
         specs = response.css("div.specs").getall()
@@ -84,7 +96,8 @@ class CasePartSpider(scrapy.Spider):
             specs_list = soup.find_all("div", {"class": "group group--spec"})
         else:
             return {}
-        
+
+        scraped_info['part_type'] = part_type
         
         # Gather the specification for this part
         for spec in specs_list:
